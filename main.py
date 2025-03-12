2580bd6a80a6e444631c3a464d344bb2cf79acb2
wipeseals <wipeseals@gmail.com>
2025-03-13 00:33:16 +0900

    refactor: debug message統一

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class NandIo:
 
     def debug(self, msg: str) -> None:
         if self.is_debug:
-            print(msg)
+            print(f"[DEBUG]\tNandIo\t{msg}")
 
     ########################################################
     # Low-level functions
@@ -69,15 +69,18 @@ class NandIo:
         return value
 
     def set_io_dir(self, is_output: bool) -> None:
+        self.debug(f"IO\t{'OUT' if is_output else 'IN'}")
         for pin in self.io:
             pin.init(Pin.OUT if is_output else Pin.IN)
 
     def set_ceb(self, cs_index: int | None) -> None:
         assert cs_index is None or cs_index in [0, 1]
         if cs_index is None:
+            self.debug("CS\tNone")
             self.ceb0.on()
             self.ceb1.on()
         else:
+            self.debug(f"CS\t{cs_index}")
             self.ceb0.value(0 if cs_index == 0 else 1)
             self.ceb1.value(0 if cs_index == 1 else 1)
 
@@ -98,6 +101,7 @@ class NandIo:
         self.reb.value(value)
 
     def setup_pin(self) -> None:
+        self.debug("SETUP")
         for pin in self.io:
             pin.init(Pin.OUT)
             pin.off()
@@ -120,12 +124,48 @@ class NandIo:
         return self.rbb.value()
 
     def init_pin(self) -> None:
+        self.debug("INIT")
         self.set_io_dir(is_output=True)
         self.set_ceb(None)
         self.set_cle(0)
         self.set_ale(0)
         self.set_web(1)
         self.set_reb(1)
+
+    def input_cmd(self, cmd: int) -> None:
+        self.debug(f"CMD\t{cmd:02X}")
+        self.set_io(cmd)
+        self.set_cle(1)
+        self.set_web(0)
+        self.seq_delay()
+        self.set_web(1)
+        self.set_cle(0)
+
+    def input_addrs(self, addrs: bytearray) -> None:
+        self.debug(f"ADDR\t{addrs.hex()}")
+        for addr in addrs:
+            self.set_io(addr)
+            self.set_ale(1)
+            self.set_web(0)
+            self.seq_delay()
+            self.set_web(1)
+            self.set_ale(0)
+
+    def input_addr(self, addr: int) -> None:
+        self.input_addrs(bytearray([addr]))
+
+    def output_data(self, num_bytes: int) -> bytearray:
+        datas = bytearray()
+        self.set_io_dir(is_output=False)
+        for i in range(num_bytes):
+            self.set_reb(0)
+            self.seq_delay()
+            datas.append(self.get_io())
+            self.set_reb(1)
+            self.seq_delay()
+        self.set_io_dir(is_output=True)
+        self.debug(f"DOUT\t{datas.hex()}")
+        return datas
 
 
 class NandCommander:
@@ -135,12 +175,12 @@ class NandCommander:
 
     def debug(self, msg: str) -> None:
         if self.is_debug:
-            print(msg)
+            print(f"[DEBUG]\tNandCmd\t{msg}")
 
     ########################################################
     # Communication functions
     ########################################################
-    def read_id(self, cs_index: int) -> bytearray:
+    def read_id(self, cs_index: int, num_bytes: int = 5) -> bytearray:
         """ID Read Commandを実行し、IDを取得する
 
         Args:
@@ -149,42 +189,20 @@ class NandCommander:
         Returns:
             bytearray: ID Readの結果
         """
-        id = bytearray()
-        self.nand.set_io_dir(is_output=True)
-        self.nand.set_cle(0)
-        self.nand.set_ale(0)
-        self.nand.set_web(1)
-        self.nand.set_reb(1)
-
+        # initialize
+        self.nand.init_pin()
         # CS select
         self.nand.set_ceb(cs_index=cs_index)
         # Command Input
-        self.nand.set_io(NandCmd.READ_ID)
-        self.nand.set_cle(1)
-        self.nand.set_web(0)
-        self.nand.seq_delay()
-        self.nand.set_web(1)
-        self.nand.set_cle(0)
+        self.nand.input_cmd(NandCmd.READ_ID)
         # Address Input
-        self.nand.set_io(0)
-        self.nand.set_ale(1)
-        self.nand.set_web(0)
-        self.nand.seq_delay()
-        self.nand.set_web(1)
-        self.nand.set_ale(0)
+        self.nand.input_addr(0)
         # ID Read
-        self.nand.set_io_dir(is_output=False)
-        self.nand.seq_delay()
-        for i in range(5):
-            self.nand.set_reb(0)
-            self.nand.seq_delay()
-            id.append(self.nand.get_io())
-            self.nand.set_reb(1)
-            self.nand.seq_delay()
-
-        self.nand.set_io_dir(is_output=True)
+        id = self.nand.output_data(num_bytes=num_bytes)
         # CS deselect
         self.nand.set_ceb(None)
+
+        self.debug(f"read_id\tcs={cs_index}\tid={id.hex()}")
 
         return id
 
@@ -204,7 +222,7 @@ class NandCommander:
         for cs_index in range(check_num_cs):
             id = self.read_id(cs_index=cs_index)
             is_ok = id == expect_id
-            self.debug(f"[CS{cs_index}]: ID:{id.hex()} {'OK' if is_ok else 'NG'}")
+            self.debug(f"check_num_active_cs\tcs={cs_index}\tis_ok={is_ok}")
             if not is_ok:
                 return num_cs
             num_cs += 1
@@ -222,9 +240,8 @@ def panic(cond: bool, msg: str) -> None:
 
 
 def main() -> None:
-    is_debug = True
-    nandio = NandIo(is_debug=is_debug)
-    nandcmd = NandCommander(nand=nandio, is_debug=is_debug)
+    nandio = NandIo(is_debug=True)
+    nandcmd = NandCommander(nand=nandio, is_debug=True)
     num_cs = nandcmd.check_num_active_cs()
     print(f"Number of active CS: {num_cs}")
     panic(num_cs == 0, "No NAND flash is found.")
